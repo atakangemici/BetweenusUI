d51a8f135f6096a2315a0f70659f76d188912b07
Atakan Gemici <atakan.gemici@jitterbit.com>
2022-11-07 19:43:50 +0300

    url fixed.

--- src/app/chat-inbox/chat-inbox.component.ts
+++ src/app/chat-inbox/chat-inbox.component.ts
@@ -1,8 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { io } from 'socket.io-client';
 
-// const SOCKET_ENDPOINT = 'https://betweenus-app.herokuapp.com';
-const SOCKET_ENDPOINT = 'http://localhost:3000';
+const SOCKET_ENDPOINT = 'https://betweenus-app.herokuapp.com';
+// const SOCKET_ENDPOINT = 'http://localhost:3000';
 
 @Component({
   selector: 'app-chat-inbox',
